70cfc05abf65dfc1b1490a39df6c6982bab148b1
aquarius <zhangpei01@bilibili.com>
2024-03-22 17:41:22 +0800

    login

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -2,16 +2,5 @@ import React from "react";
 import { Button, message } from "antd";
 
 export default function Login() {
-  const [messageApi, contextHolder] = message.useMessage();
-
-  return (
-    <div>
-      {contextHolder}
-      <Button
-        onClick={() => {
-          messageApi.info("test");
-        }}
-      ></Button>
-    </div>
-  );
+  return <div></div>;
 }
