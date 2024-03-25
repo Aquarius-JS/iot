123a6c94990df432232daec764fe375f0a2aa843
aquarius <zhangpei01@bilibili.com>
2024-03-25 17:54:47 +0800

    layout

--- src/views/Layout/index.tsx
+++ src/views/Layout/index.tsx
@@ -1,6 +1,57 @@
+import { Layout } from "antd";
 
-export default function LayOut() {
+const { Header, Sider, Content, Footer } = Layout;
+
+export default function Index() {
+  const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 48,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+  };
+  
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#0958d9',
+  };
+  
+  const siderStyle: React.CSSProperties = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#1677ff',
+  };
+  
+  const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#4096ff',
+  };
+  
+  const layoutStyle = {
+    borderRadius: 8,
+    overflow: 'hidden',
+    width: 'calc(50% - 8px)',
+    maxWidth: 'calc(50% - 8px)',
+  };
+
   return (
-    <div>LayOut</div>
-  )
+    <Layout style={layoutStyle}>
+      <Layout>
+        <Sider width="25%" style={siderStyle}>
+          Sider
+        </Sider>
+        <Layout>
+          <Header style={headerStyle}>Header</Header>
+          <Content style={contentStyle}>Content</Content>
+        </Layout>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  );
 }
