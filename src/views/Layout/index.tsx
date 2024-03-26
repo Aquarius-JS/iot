506d7f3174e874d050b11093e6dbf9af68a34b55
aquarius <zhangpei01@bilibili.com>
2024-03-26 15:29:29 +0800

    layout

--- src/views/Layout/index.tsx
+++ src/views/Layout/index.tsx
@@ -1,53 +1,66 @@
 import { Layout } from "antd";
+import { useState } from "react";
+import { Button } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
 
 const { Header, Sider, Content, Footer } = Layout;
 
 export default function Index() {
   const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
     height: 64,
     paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
-  };
-  
-  const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    minHeight: 120,
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#0958d9',
-  };
-  
-  const siderStyle: React.CSSProperties = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
-  };
-  
-  const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#4096ff',
-  };
-  
-  const layoutStyle = {
-    borderRadius: 8,
-    overflow: 'hidden',
-    width: 'calc(50% - 8px)',
-    maxWidth: 'calc(50% - 8px)',
+    lineHeight: "64px",
+    backgroundColor: "#4096ff",
   };
 
+  const contentStyle: React.CSSProperties = {
+    textAlign: "center",
+    minHeight: "80vh",
+    lineHeight: "120px",
+    color: "#fff",
+    backgroundColor: "#0958d9",
+  };
+
+  const footerStyle: React.CSSProperties = {
+    textAlign: "center",
+    color: "#fff",
+    backgroundColor: "#4096ff",
+  };
+
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <Layout style={layoutStyle}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Layout>
-        <Sider width="25%" style={siderStyle}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          style={{ backgroundColor: "white" }}
+        >
           Sider
         </Sider>
         <Layout>
-          <Header style={headerStyle}>Header</Header>
+          <Header style={{ padding: 0, backgroundColor: "white" }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
           <Content style={contentStyle}>Content</Content>
         </Layout>
       </Layout>
