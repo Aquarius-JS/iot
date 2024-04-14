217ab5d016316fc3374a4e86946957ee780b73d4
aquariusjs <1015772292@qq.com>
2024-04-14 12:22:04 +0800

    人脸识别

--- src/views/Attendance/index.tsx
+++ src/views/Attendance/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import * as faceapi from "face-api.js";
+import { message } from "antd";
 
 export default function Attendance() {
+	const [messageApi, contextHolder] = message.useMessage();
 	useEffect(() => {
 		async function run() {
 			const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
@@ -41,25 +43,40 @@ export default function Attendance() {
 				.withFaceLandmarks()
 				.withFaceDescriptors();
 			if (result.length === 0) {
-				_t = setTimeout(() => onPlay(), 4000);
+				_t = setTimeout(() => onPlay(), 3000);
 				return;
 			}
 			const faceMatcher = new faceapi.FaceMatcher(result);
-			const test = await faceapi
-				.detectSingleFace(testImg)
-				.withFaceLandmarks()
-				.withFaceDescriptor();
-			const best = faceMatcher.findBestMatch(test?.descriptor);
-			_t = setTimeout(() => onPlay(), 4000);
+			let bestMatcher = [1, null];
+			for (let i of ["张培.jpg", "张鹏.png"]) {
+				const img = document.createElement("img");
+				img.src = `/${i}`;
+				img.id = i;
+				const test = await faceapi
+					.detectSingleFace(img)
+					.withFaceLandmarks()
+					.withFaceDescriptor();
+				const best = faceMatcher.findBestMatch(test?.descriptor);
+				bestMatcher =
+					bestMatcher[0] > best.distance ? [best.distance, i.split(".")[0]] : bestMatcher;
+			}
+			if (bestMatcher[0] < 0.4) {
+				const content = bestMatcher[1] + "打卡成功, " + new Date();
+				messageApi.open({
+					type: "success",
+					content,
+				});
+			}
+			_t = setTimeout(() => onPlay(), 3000);
 		}
 		onPlay();
 	}, []);
 	return (
 		<div style={{ padding: "20px" }}>
+			{contextHolder}
 			<div style={{ position: "relative" }}>
 				<video id="video" width={500} autoPlay></video>
 			</div>
-			<img id="te" src="/张培.jpg" alt="test" style={{ display: "none" }} />
 		</div>
 	);
 }
